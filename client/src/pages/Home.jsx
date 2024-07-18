722901f323b10032c559950f183a01ac07a1c593
Av <yungvade1@gmail.com>
2024-07-18 17:14:38 +0100

    fir

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -71,7 +71,7 @@ const Home = () => {
   return (
     <Container>
       <Section>
-        <Img src={HeaderImage} />
+        <Img src="https://plus.unsplash.com/premium_photo-1661526833843-248a2f8fe129?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </Section>
 
       <Section>
